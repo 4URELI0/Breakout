d2fcd1fb0ee27e9986148946733a23ecf9ba3371
ArrayaQuispeCristianJavier <arrayacristian75@gmail.com>
2023-11-01 12:37:04 -0300

    Se agrego comentario en AnimationEvent

--- Assets/Scripts/AnimationEvents.cs
+++ Assets/Scripts/AnimationEvents.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AnimationEvents : MonoBehaviour
 {
-    /*Se encargar de destruir la animacion*/
+    /*Se encargar de destruir la animación al chocar con un bloque*/
     public void DestroyExplosion()
     {
         Destroy(gameObject);
