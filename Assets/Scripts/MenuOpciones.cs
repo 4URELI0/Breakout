32dc290e5cb60fe1989f2451d2fe4cbd52ace6d5
ArrayaQuispeCristianJavier <arrayacristian75@gmail.com>
2023-11-05 12:47:17 -0300

    Se agrego funcionamiento al controlador de volumen con su script

---
+++ Assets/Scripts/MenuOpciones.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MenuOpciones : MonoBehaviour
+{
+    [SerializeField] AudioMixer audioMixer;
+    public void changeVolume(float volumen)
+    {
+        audioMixer.SetFloat("Volumen", volumen);
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
